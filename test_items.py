e7d0718ce526068b4ae11a46225660c2ad1f2e1a
SilAleks <alek.tube@gmail.com>
2023-03-06 03:07:30 +0700

    Задание: запуск автотестов для разных языков интерфейса

--- test_items.py
+++ test_items.py
@@ -5,12 +5,3 @@ def test_btn_add_to_basket(browser):
     time.sleep(30)
     btn_add_to_basket = browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
     assert str((type(btn_add_to_basket))) == "<class 'selenium.webdriver.remote.webelement.WebElement'>", "\n Нема кнопки !"
-
-from selenium.webdriver.common.by import By # Для поиска по селекторам
-import time
-
-def test_btn_add_to_basket(browser):
-    time.sleep(30)
-    btn_add_to_basket = browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
-    assert str((type(btn_add_to_basket))) == "<class 'selenium.webdriver.remote.webelement.WebElement'>", "\n Нема кнопки !"
-
