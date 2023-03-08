8c34f0d8ca356f46b38c4dbc65bcc578d7ef6765
SilAleks <alek.tube@gmail.com>
2023-03-09 04:56:33 +0700

    Подготовка файлов к Page Object

--- conftest.py
+++ conftest.py
@@ -3,17 +3,18 @@ from selenium import webdriver
 import time
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default="ru",
+    parser.addoption('--language', action='store', default="en",
                      help="Choose language: 'es' or 'fr'",
-                     choices=("es", "fr"))
+                     choices=("es", "fr", "en"))
 
 @pytest.fixture(scope="function")
 def browser(request):
     print("\nStart browser..")
     language = request.config.getoption("language")
     browser = webdriver.Chrome()
-    browser.get(f"https://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/")
+    #browser.get(f"https://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/")
+    browser.get(f"https://selenium1py.pythonanywhere.com/{language}")
     yield browser
     print("\nquit browser..")
-    #time.sleep(3)
+    time.sleep(5)
     browser.quit()
